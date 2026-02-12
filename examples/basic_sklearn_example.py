d5430ea4d532ffe585e75f51fbc6f1ac02cc1ab2
Kate Lonsway <klons06@login1.int.fir.alliancecan.ca>
2026-02-12 07:13:13 -0800

    automatically loading my python ENV, trying to remove partition

--- examples/basic_sklearn_example.py
+++ examples/basic_sklearn_example.py
@@ -58,9 +58,9 @@ def main():
             additional_metrics=["f1_macro", "precision_macro", "recall_macro"],
         ),
         slurm=SlurmConfig(
-#            partition="",
+            partition="cpubase_bynode_b5",
             time="00:30:00",
-            memory="4096M",
+            memory="4G",
             cpus_per_task=2,
             email_type="FAIL",
         )
