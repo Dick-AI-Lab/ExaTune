72c8e6a2ac699f8a051c99473e2bdede427c9daf
Katie Lonsway <klons06@login1.int.fir.alliancecan.ca>
2026-03-12 08:07:05 -0700

    second run of pregnancy outcome ds

--- examples/scripts/benchmarks/run_classification_benchmarks.py
+++ examples/scripts/benchmarks/run_classification_benchmarks.py
@@ -34,9 +34,9 @@ CLASSIFICATION_BENCHMARKS = {
         "grid_size_estimate": 15120,  # 5 * 6 * 7 * 5 * 6 * 6 * 5 * 5 * 5
     },
 "pregnancy_outcome": {
-        "config": "configs/benchmarks/pregnancy_outcome_calssification.yaml",
-        "description": "My custom dataset - Random Forest",
-        "grid_size_estimate": 180,
+        "config": "configs/born_air/pregnancy_outcome_calssification.yaml",
+        "description": "My custom dataset - XGB from born air",
+        "grid_size_estimate": 5184,
     },
 }
 
