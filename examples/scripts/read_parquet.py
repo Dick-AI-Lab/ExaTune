525d63c739fe1f2ad9e08618ee1b06a6896567ea
katie-060330 <kate.lonsway@outlook.com>
2026-03-31 17:03:50 -0400

    adding a readable parquet script

--- examples/scripts/read_parquet.py
+++ examples/scripts/read_parquet.py
@@ -1,6 +1,16 @@
-# Source - https://stackoverflow.com/a/47036357
-# Posted by chrisaycock, modified by community. See post 'Timeline' for change history
-# Retrieved 2026-03-25, License - CC BY-SA 4.0
-
+import argparse
 import pandas as pd
 
+parser = argparse.ArgumentParser()
+parser.add_argument('--path', required=True, help='Path to the parquet file')
+parser.add_argument('--new_path', required=False, help='path to new file')
+args = parser.parse_args()
+
+df = pd.read_parquet(args.path)
+
+if(args.new_path):
+    to_csv = df.to_csv(args.new_path)
+
+print(df.head())
+print(df.columns.tolist())
+print( df.dtypes)
