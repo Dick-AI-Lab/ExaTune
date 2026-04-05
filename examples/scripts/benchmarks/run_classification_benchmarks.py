8e618baa69ec1e36f9ada58f619508a5583a9439
katie-060330 <kate.lonsway@outlook.com>
2026-04-05 11:23:42 -0400

    adding msist

--- examples/scripts/benchmarks/run_classification_benchmarks.py
+++ examples/scripts/benchmarks/run_classification_benchmarks.py
@@ -88,6 +88,12 @@ CLASSIFICATION_BENCHMARKS = {
     "grid_size_estimate": 900,
 
 },
+"mn": {
+        "config": "configs/neurips/mnist_classification.yaml",
+        "description": "My custom dataset - XGB from born air",
+    "grid_size_estimate": 900,
+
+},
 }
 
 def run_benchmark(benchmark_name: str, submit: bool = False, dry_run: bool = False):
